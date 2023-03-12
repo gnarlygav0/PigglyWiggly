aa18fbd6a763509565e12c3a69efd231e703d67c
gnarlygav0 <queue.gavin@gmail.com>
2023-03-12 07:33:12 -0700

    Initial Commit

---
+++ Piggly WigglyUITests/Piggly_WigglyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Piggly_WigglyUITestsLaunchTests.swift
+//  Piggly WigglyUITests
+//
+//  Created by Gavin Olsen on 3/12/23.
+//
+
+import XCTest
+
+class Piggly_WigglyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
